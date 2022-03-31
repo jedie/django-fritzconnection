4bffa393bfac253689afc6809b8cfe13c4c9fa7b
JensDiemer <git@jensdiemer.de>
2022-03-31 16:36:27 +0200

    Catch and log FritzConnectionException

--- djfritz/fritz_connection.py
+++ djfritz/fritz_connection.py
@@ -1,17 +1,25 @@
+import logging
+
 from django.utils import timezone
 from fritzconnection import FritzConnection
+from fritzconnection.core.exceptions import FritzConnectionException
+
+
+logger = logging.getLogger(__name__)
 
 
 class LazyFritzConnection:
     fc = None
-    last_connection=None
-
-
+    last_connection = None
 
     def __call__(self) -> FritzConnection:
         if self.fc is None:
-            self.fc = FritzConnection()
-            self.last_connection = timezone.now()
+            try:
+                self.fc = FritzConnection()
+            except FritzConnectionException as err:
+                logger.error('Can not connect to FritzBox: %s', err)
+            else:
+                self.last_connection = timezone.now()
         return self.fc
 
 
