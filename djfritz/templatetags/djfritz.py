5d94314e10f858f92ba49e04b221f229fd17806a
JensDiemer <git@jensdiemer.de>
2022-03-31 16:37:00 +0200

    Add FritzBox connection information in admin header

--- djfritz/templatetags/djfritz.py
+++ djfritz/templatetags/djfritz.py
@@ -1,7 +1,9 @@
 from django import template
 from django.template.loader import render_to_string
 
+from djfritz.fritz_connection import get_fritz_connection
 from djfritz.views import dynamic_view_menu
+
 
 register = template.Library()
 
@@ -12,3 +14,17 @@ def view_menu(context):
         'menu': dynamic_view_menu.menu,
     }
     return render_to_string('djfritz/includes/dynamic_view_menu.html', context)
+
+
+@register.simple_tag(takes_context=True)
+def fritzbox_link(context):
+    fc = get_fritz_connection()
+    if not fc:
+        return '<not connected>'
+
+    context = {
+        'modelname': fc.modelname,
+        'address': fc.soaper.address,
+        'system_version': fc.system_version,
+    }
+    return render_to_string('djfritz/includes/fritzbox_link.html', context)
